1b9d9d63ceb6d7043520bbcb5a97f6b7cce1c8a3
nagachen <birdann99@gmail.com>
2023-07-11 18:39:39 +0800

    index 和 backend .php  ok

--- backend.php
+++ backend.php
@@ -1,5 +1,4 @@
-﻿<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0068)?do=admin&redo=title -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -15,8 +14,7 @@
 <body>
 	<div id="cover" style="display:none; ">
 		<div id="coverr">
-			<a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;"
-				onclick="cl(&#39;#cover&#39;)">X</a>
+			<a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl(&#39;#cover&#39;)">X</a>
 			<div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
 		</div>
 	</div>
@@ -30,8 +28,7 @@
 				<div id="menuput" class="dbor">
 					<!--主選單放此-->
 					<span class="t botli">後台管理選單</span>
-					<a style="color:#000; font-size:13px; text-decoration:none;"
-						href="./Management page_files/Management page.htm">
+					<a style="color:#000; font-size:13px; text-decoration:none;" href="./Management page_files/Management page.htm">
 						<div class="mainmu">
 							網站標題管理 </div>
 					</a>
@@ -76,28 +73,37 @@
 				</div>
 			</div>
 
-			<div id="alt"
-				style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
+			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
 			</div>
 			<script>
 				$(".sswww").hover(
-					function () {
-						$("#alt").html("" + $(this).children(".all").html() + "").css({ "top": $(this).offset().top - 50 })
+					function() {
+						$("#alt").html("" + $(this).children(".all").html() + "").css({
+							"top": $(this).offset().top - 50
+						})
 						$("#alt").show()
 					}
 				)
 				$(".sswww").mouseout(
-					function () {
+					function() {
 						$("#alt").hide()
 					}
 				)
 			</script>
 		</div>
-		
-	
+
+		<?php 
+		$do=$_GET['do']??'title';
+		$files="./view/".$do.".php";
+		if(file_exists($files)){
+			include "$files";
+		}else{
+			include "./view/title.php"; 
+
+		}
+		?>
 		<div style="clear:both;"></div>
-		<div
-			style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
+		<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
 			<span class="t" style="line-height:123px;"></span>
 		</div>
 	</div>
