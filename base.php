40d95e4f792b60b968b9c190535b0587bb2c4c18
nagachen <birdann99@gmail.com>
2023-07-11 22:35:16 +0800

    建立常用的函式在base.php

--- base.php
+++ base.php
@@ -12,6 +12,15 @@
         echo "</pre>";
     }
 
+    function to($url){
+        header("location:$url");
+    }
+    function q($sql){
+        $dsn="mysql:host=localhost;charset=utf8;dbname=db02";
+        $pdo=new PDO($dsn,'root','');
+        $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
     $title=new Title;
 //    dd($title->save(['id'=>'2','text'=>'aaaa222']));
 
